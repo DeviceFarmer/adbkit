256f4a45a33a08a1d946a89451e27b365ffe0d2e
Sergey Volkov <s.volkov@netris.ru>
2020-10-13 18:31:52 +0300

    lint: disable no-explicit-any
    Signed-off-by: Sergey Volkov <s.volkov@netris.ru>

--- lib/adb/command/host/trackdevices.ts
+++ lib/adb/command/host/trackdevices.ts
@@ -4,6 +4,8 @@ import HostDevicesCommand from './devices';
 import Bluebird from 'bluebird';
 
 class HostTrackDevicesCommand extends HostDevicesCommand {
+	// FIXME(intentional any): correct return value: `Bluebird<Tracker>`
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	execute(): Bluebird<any> {
 		this._send('host:track-devices');
 		return this.parser.readAscii(4).then((reply) => {
